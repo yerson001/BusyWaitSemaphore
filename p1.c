c41bd7b0dceb0cce09f00371bcc7e7cc11f2963d
yerson001 <yhon.sanchez@ucsp.edu.pe>
2020-11-14 08:28:55 -0500

    firts

---
+++ p1.c
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define FORN(i, a, b) for(int i = (a); i < (b); i++)
+#define FORN_(i,a,n) for(int (i)=(a);(i)<=(n);(i)++)
+#define debug(a) cout<<#a<<" = "<<a<<endl;
+#define END cout<<endl;
+
+int main()
+{
+    return 0;
+}
+
